6c2075f51fe59beee59b56e914cb701e97e9a585
Timofey Saikin <84962721+Triler1@users.noreply.github.com>
2023-03-27 00:34:42 +0400

    Add files via upload

--- js/album.js
+++ js/album.js
@@ -1,8 +1,32 @@
-let container = document.querySelector(`.img-text`);
+let container = document.querySelector(`.album`);
+let playlist = document.querySelector(`.playlist`);
+
+let album = albums[0];
+
+let tracks = album.tracks;
 
 container.innerHTML = `
-<img src="${albums[0].img}" class="img">
+<img src="${album.img}" class="img">
 <div class="body-1" style="left: 30px;">
-    <p class="card-text">${albums[0].title}</p>
+    <p class="card-text">${album.title}</p>
 </div>
 `;
+
+for (let j = 0; j < tracks.length; j++) {
+    let track = tracks[j];
+    playlist.innerHTML += `
+    <li class="d-flex align-items-center mb-3">
+        <span class="me-3">0${j}</span>
+        <img class="me-3" src="${track.img}" width="50px" height="50px">
+        <div class="text me-5">
+            <div>${track.title}</div>
+            <div class="d-flex">
+                <img src="assets/profile-no.png" width="12px" height="12px">
+                <div class="text-secondary ms-1">${track.author}</div>
+            </div>
+        </div>
+        <span class="time ms-2">${track.time}</span>
+        <span class="time text-secondary ms-auto me-4">${track.more}</span>
+    </li>
+    `;
+}
--- js/data.js
+++ js/data.js
@@ -5,42 +5,50 @@ let albums = [{
         title: `Heat Waves`,
         author: `Glass Animals`,
         time: `03:29`,
-        more: `508 000 000`
+        more: `508 000 000`,
+        img: `assets/heatwaves.png`
     }, {
         title: `I'm Good (Blue)`,
         author: `David Guetta & Bebe Rexha`,
         time: `02:56`,
-        more: `139 000 000`
+        more: `139 000 000`,
+        img: `assets/good.jpg`
     }, {
         title: `Under the Influence`,
         author: `Chris Brown`,
         time: `03:04`,
-        more: `136 000 000`
+        more: `136 000 000`,
+        img: `assets/under.jpeg`
     }, {
         title: `Resistance`,
         author: `Muse`,
         time: `05:45`,
-        more: `124 000 000`
+        more: `124 000 000`,
+        img: `assets/resistance.jpg`
     }, {
         title: `Glimpse of Us`,
         author: `Joji`,
         time: `03:54`,
-        more: `44 000 000`
+        more: `44 000 000`,
+        img: `assets/us.jpg`
     }, {
         title: `Forget Me`,
         author: `Lewis Capaldi`,
         time: `03:38`,
-        more: `2 700 000`
+        more: `2 700 000`,
+        img: `assets/forget.png`
     }, {
         title: `Bad Habit`,
         author: `Steve Lacy`,
         time: `03:32`,
-        more: `1 934 291`
+        more: `1 934 291`,
+        img: `assets/bad.jpg`
     }, {
         title: `DON'T YOU WORRY`,
         author: `Black Eyed Peas, Shakira`,
         time: `03:42`,
-        more: `1 556 239`
+        more: `1 556 239`,
+        img: `assets/you.jpg`
     }]
 }, {
     title: `Собраться с мыслями`,
